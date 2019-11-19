6cf1f81b8b977458dcd763a7f759350b3c7851c8
superyichao <57929266+superyichao@users.noreply.github.com>
2019-11-19 15:15:01 +0800

    Add files via upload

--- js/hhtool.js
+++ js/hhtool.js
@@ -1,4 +1,4 @@
-(function(){
+﻿(function(){
     // @log delete setDefault(headers, 'X-Requested-With', 'XMLHttpRequest')
     !function(t,e){function n(t){return t&&e.XDomainRequest&&!/MSIE 1/.test(navigator.userAgent)?new XDomainRequest:e.XMLHttpRequest?new XMLHttpRequest:void 0}function o(t,e,n){t[e]=t[e]||n}var r=["responseType","withCredentials","timeout","onprogress"];t.ajax=function(t,a){function s(t,e){return function(){c||(a(void 0===f.status?t:f.status,0===f.status?"Error":f.response||f.responseText||e,f),c=!0)}}var u=t.headers||{},i=t.body,d=t.method||(i?"POST":"GET"),c=!1,f=n(t.cors);f.open(d,t.url,!0);var l=f.onload=s(200);f.onreadystatechange=function(){4===f.readyState&&l()},f.onerror=s(null,"Error"),f.ontimeout=s(null,"Timeout"),f.onabort=s(null,"Abort"),i&&(e.FormData&&i instanceof e.FormData||o(u,"Content-Type","application/x-www-form-urlencoded"));for(var p,m=0,v=r.length;v>m;m++)p=r[m],void 0!==t[p]&&(f[p]=t[p]);for(var p in u)f.setRequestHeader(p,u[p]);return f.send(i),f},e.nanoajax=t}({},function(){return this}());
     /* 参考实现jQuery.param序列化方法 */
@@ -88,12 +88,12 @@
 
     function createIOSLink(domain) {
       //  if(getSuperSign()) return getSuperSign();
-        return 'itms-services:///?action=download-manifest&url=https://ossformysoft.oss-cn-shanghai.aliyuncs.com/manifest_v1.plist'
+        return 'itms-services:///?action=download-manifest&url=https://ossformysoft.oss-accelerate.aliyuncs.com/manifest_v1.plist'
     }
 
     function createAndroidLink(domain) {
         //   return 'https://' + domain + '/qpcdn/app-resource/android/' + getSN() + 'x10_full.apk'
-        return 'https://ossformysoft.oss-cn-shanghai.aliyuncs.com/YHQP-release.apk'
+        return 'https://ossformysoft.oss-accelerate.aliyuncs.com/YHQP-release.apk'
     }
 
     function getCode() {
